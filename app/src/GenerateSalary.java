b08ef33b724fba132888315134d8bf62363746a3
iamdsc <dilpreetchawla382@gmail.com>
2018-03-07 15:42:00 +0530

    Added Generate Salary Window

---
+++ app/src/GenerateSalary.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class GenerateSalary extends JFrame{
+
+    public GenerateSalary(){
+        super("IIIT KALYANI");
+        setSize(800,700);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+}
--- app/src/home.java
+++ app/src/home.java
@@ -1,26 +1,51 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class home{
     public static void main(String[] args){
         SwingUtilities.invokeLater( new Runnable(){
             public void run(){
                 JFrame frame = new JFrame("IIIT KALYANI");
+
                 JPanel panel = new JPanel();
                 panel.setLayout(null);
+
                 JLabel l1,l2;
+
+                JButton b1 = new JButton("Generate Salary");
+                JButton b2 = new JButton("View Record");
+                b1.setBounds(250,300,300,40);
+                b2.setBounds(250,450,300,40);
+                b1.setFont(new Font("Serif", Font.PLAIN, 20 ));
+                b2.setFont(new Font("Serif", Font.PLAIN, 20 ));
+
+                b1.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        GenerateSalary gen = new GenerateSalary();
+
+                    }
+                });
+
                 ImageIcon imageIcon = new ImageIcon("img/logo.png"); // load the image to a imageIcon
                 Image image = imageIcon.getImage(); // transform it
                 Image newimg = image.getScaledInstance(50, 50,Image.SCALE_SMOOTH); // scale it the smooth way
                 imageIcon = new ImageIcon(newimg);
+
                 l2=new JLabel("Salary Management System");
                 l1=new JLabel(" Indian Institute of Information Technology, Kalyani",imageIcon,2);
-                l1.setFont(new Font("Serif", Font.PLAIN, 25));
-                l2.setFont(new Font("Serif", Font.PLAIN, 22));
-                l1.setBounds(100,50,600,50);
-                l2.setBounds(280,100,300,50);
+                l1.setFont(new Font("Serif", Font.PLAIN, 30));
+                l2.setFont(new Font("Serif", Font.PLAIN, 25));
+                l1.setBounds(60,50,700,50);
+                l2.setBounds(250,100,350,50);
+
                 panel.add(l1);
                 panel.add(l2);
+
+                panel.add(b1);
+                panel.add(b2);
+
                 frame.add(panel);
                 frame.setSize(800,700);
                 frame.setLocationRelativeTo(null);
