1d8e371bcb178e6aad981e8c442d8dcbd3010835
iamdsc <dilpreetchawla382@gmail.com>
2018-03-16 09:46:00 +0530

    Added Update Employee Record

--- SalaryManagementProject/src/gui/home.java
+++ SalaryManagementProject/src/gui/home.java
@@ -24,26 +24,30 @@ public class home extends JFrame{
 
                 JButton b1 = new JButton("Generate Current Salary");
                 JButton b2 = new JButton("View Yearly Record");
+                JButton b3 = new JButton("Update Employee Record");
 
                 b1.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
                 b1.setForeground(new Color(0, 0, 0));
                 b1.setBackground(new Color(240, 248, 255));
-
-                b2.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-                b2.setForeground(new Color(0, 0, 0));
-                b2.setBackground(new Color(240, 248, 255));
-
                 b1.setBounds(250,349,267,40);
-                b2.setBounds(250,444,267,40);
-                b1.setFont(new Font("Nirmala UI Semilight", Font.BOLD, 20));
-                b2.setFont(new Font("Nirmala UI Semilight", Font.BOLD, 20));
+                b1.setFont(new Font("Serif", Font.BOLD, 20));
+                panel.add(b1);
+
 
                 b1.addActionListener(new ActionListener() {
                     public void actionPerformed(ActionEvent e) {
                         DAO dao = new DAO();
                         dao.getEmployee();
                     }
-                });
+                 });
+
+                b2.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+                b2.setForeground(new Color(0, 0, 0));
+                b2.setBackground(new Color(240, 248, 255));
+                b2.setBounds(250,444,267,40);
+                b2.setFont(new Font("Serif", Font.BOLD, 20));
+                panel.add(b2);
+
                 b2.addActionListener(new ActionListener() {
                     public void actionPerformed(ActionEvent e) {
                         DAO_2 dao2 = new DAO_2();
@@ -52,15 +56,22 @@ public class home extends JFrame{
                     }
                 });
 
+                b3.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+                b3.setForeground(new Color(0, 0, 0));
+                b3.setBackground(new Color(240, 248, 255));
+                b3.setBounds(250,535,267,40);
+                b3.setFont(new Font("Serif", Font.BOLD, 18));
+                panel.add(b3);
+
                 l2=new JLabel("Salary Management System");
+                l2.setForeground(new Color(255, 255, 255));
+                l2.setFont(new Font("Tw Cen MT", Font.BOLD | Font.ITALIC, 27));
+                l2.setBounds(236,244,294,62);
 
                 l1=new JLabel("Indian Institute of Information Technology, Kalyani");
                 l1.setForeground(new Color(255, 255, 255));
-                l2.setForeground(new Color(255, 255, 255));
                 l1.setFont(new Font("Serif", Font.BOLD, 27));
-                l2.setFont(new Font("Tw Cen MT", Font.BOLD | Font.ITALIC, 27));
                 l1.setBounds(90,37,750,62);
-                l2.setBounds(236,244,294,62);
                 panel.add(l1);
                 panel.add(l2);
 
@@ -70,9 +81,6 @@ public class home extends JFrame{
                 label.setIcon(imageIcon);
                 label.setBounds(335, 111, 100, 106);
                 panel.add(label);
-
-                panel.add(b1);
-                panel.add(b2);
 
                 add(panel);
                 setSize(800,700);
